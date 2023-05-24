055384ad46b7a1e510f513c05aa5c58a96bafdd4
J Mandon <78100569+LunarBullet@users.noreply.github.com>
2023-05-24 08:53:08 -0500

    Update Health.cs

--- Assets/Scrips/Health.cs
+++ Assets/Scrips/Health.cs
@@ -13,7 +13,7 @@ public class Health : MonoBehaviour
     [PunRPC]
     public void TakeDamage(int _damage)
     {
-        health -= _damage;
+        health -= _damage*2;
 
         healthText.text = health.ToString();
 
